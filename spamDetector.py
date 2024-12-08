71591515335e6ee0d243b1ffaabea62432c65606
Yaseen <notsolvablehat@gmail.com>
2024-12-08 13:15:27 +0530

    Final

---
+++ spamDetector.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open("spam.pkl",'rb'))
+cv = pickle.load(open("vec.pkl",'rb'))
+
+def main():
+    st.title("Email Spam Classification")
+    st.write("This is a machine learinig algorithm to classify emails as spam or not spam")
+    st.subheader("Classfication")
+    user_input = st.text_area("Enter email to classify", height=150)
+    if st.button("Classify"):
+        if(user_input):
+            data = [user_input]
+            print(data)
+            vec = cv.transform(data).toarray()
+            result = model.predict(vec)
+            if result[0] == 0:
+                st.success("This is not a spam email")
+            else:
+                st.error("This is a spam email")
+        else:
+            st.write("Please enter an email to classify")
+main()
